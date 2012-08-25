d04254a57aa8b391779a3a17f911a86487322d1b
Brian Zwahr <echosa@gmail.com>
2012-08-25 10:26:49 -0500

    Added basic model for the game board.

---
+++ SlideGame/SlideGame.h
@@ -0,0 +1,17 @@
+//
+//  SlideGame.h
+//  SlideGame
+//
+//  Created by Brian Zwahr on 8/25/12.
+//  Copyright (c) 2012 echosa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SlideGame : NSObject
+{
+    NSInteger gameArray[4][4];
+}
+- (void) setPosition: (NSInteger)x : (NSInteger)y : (NSInteger)value;
+- (NSInteger) getPosition: (NSInteger)x : (NSInteger)y;
+@end
---
+++ SlideGame/SlideGame.m
@@ -0,0 +1,23 @@
+//
+//  SlideGame.m
+//  SlideGame
+//
+//  Created by Brian Zwahr on 8/25/12.
+//  Copyright (c) 2012 echosa. All rights reserved.
+//
+
+#import "SlideGame.h"
+
+@implementation SlideGame
+
+- (void) setPosition: (NSInteger)x : (NSInteger)y : (NSInteger)value
+{
+    gameArray[x][y] = value;
+}
+
+- (NSInteger) getPosition: (NSInteger)x : (NSInteger)y
+{
+    return gameArray[x][y];
+}
+
+@end
